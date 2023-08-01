edfbe034cd74c4015745a5f8b7975d45112de146
Elena Lomakina <lomakina0612@gmail.com>
2023-08-02 00:00:26 +0200

    try except

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-name = input("What is your name? ")
-print(f"Hello {name}")
+val = '-1'
+try:
+    val = int(val)
+except ValueError:
+    print(f"val {val} is not a number")
+else:
+    print(val > 0)
+finally:
+    print("This will be printed anyway")
